9862aedcd0301ba2cbb9b2607cebf03196fcddba
Jão <joao_bernrdi@outlook.com>
2020-03-07 16:39:06 -0300

    Added events support

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # Webpy by Bernard
 import socket
+import os
+import ssl
 import threading
 from urllib.parse import unquote
 import traceback
@@ -49,7 +51,6 @@ def execute(code, _globals={}, _locals={}):
 	__stdout = sys.stdout
 	sys.stdout = fake_stdout
 	try:
-		#try if this is expressions
 		ret = eval(code, _globals, _locals)
 		result = fake_stdout.getvalue()
 		sys.stdout = __stdout
@@ -68,7 +69,7 @@ def execute(code, _globals={}, _locals={}):
 			sys.stdout = __stdout
 			return fake_stdout.getvalue()
 
-code = ""
+
 def pythonfier(contents, vars={}):
 	code = contents
 	cookie = ""
@@ -99,12 +100,27 @@ class WebServer(object):
 		else:
 			self.host = host
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#Content dir treatment
+		if content_dir.startswith("\\"): content_dir = content_dir[1:]
+		if not content_dir.endswith("\\") and len(content_dir) > 0: content_dir += "\\"
 		self.content_dir = content_dir
+		
+		#A masterpiece
+		class events(object):
+			def __init__(self):
+				self.hooks = {"requests": [], "requests_handler": {}}
+			def request_input(self, function):			
+				self.hooks["requests"].append(function)
+			def request_handler(self, type):
+				def wrapper(function):		
+					self.hooks["requests_handler"][type.upper()] = function
+				return wrapper
+		self.events = events()
 
 	def _headers(self, status, cookie=""):
-		preset = f"\nServer: Webpy by Bernard\nConnection: close\n\n"
+		preset = f"\nServer: Webpy\nConnection: close\n\n"
 		if cookie != "":
-			preset = f"\nServer: Webpy by Bernard\nConnection: close\n{cookie}\n\n"
+			preset = f"\nServer: Webpy\nConnection: close\n{cookie}\n\n"
 		if status == 200:
 			header = "HTTP/1.1 200 Response OK" + preset
 		elif status == 401:
@@ -129,15 +145,34 @@ class WebServer(object):
 							vars["cookies"].update({cokizinho.split("=")[0]: cokizinho.split("=")[1][:-1]})
 						else:
 							vars["cookies"].update({cokizinho.split("=")[0]: cokizinho.split("=")[1]})
-		file = body.split(" ")[1].split("?")[0]
+		
 		try:
 			for param in body.split(" ")[1].split("?")[1].split("&"):
 				vars["url_params"].update({param.split("=")[0]: param.split("=")[1]})
 		except:
 			pass
-		file = self.content_dir + file
-		if type in ["GET", "HEAD"]:
-			if file == self.content_dir + "/": file = self.content_dir + "index.html"
+		# Filename treatment
+
+		file = body.split(" ")[1].split("?")[0]
+		if file.startswith("/"):
+			if len(file) == 1: file = "index.html"
+			else:
+				file = file[1:]
+			file = self.content_dir + file.replace("/", "\\")
+			if not file.endswith("\\") and os.path.isdir(file):
+				if os.path.isdir(file+"\\"):
+					file += "\\"
+					return b"HTTP/1.1 302 Path Realocation\nConnection: Close\nLocation: "+file.replace("\\", "/").encode("utf-8")+b"\nServer: Stronghold Proxy\n\n"
+			if os.path.isdir(file) and file.endswith("\\"):
+				file += "\\index.html"
+		
+		for event_function in self.events.hooks["requests"]:
+			event_function(type, body, addr, file, vars["cookies"])
+		
+		if type in self.events.hooks["requests_handler"]:
+			return self.events.hooks["requests_handler"][type](type, body, addr, file, vars["cookies"], vars["url_params"])
+		
+		elif type in ["GET", "HEAD"]:
 			try:
 				file_contents = htmlfy(open(file, "rb").read())
 				if file.endswith(".html"): cookies, file_contents = pythonfier(file_contents.decode(), vars)
@@ -147,10 +182,10 @@ class WebServer(object):
 					404).encode() + b"<html><head><title>404</title></head><body><center><h1>Erro 404</h1></center></body></html>"
 			except OSError:
 				return self._headers(403).encode() + htmlfy(
-					f"<html><head><title>UC | 403</title></head><body><center><h1>Erro 403</h1><br><p>Esta p&aacutegina &eacute restrita.</p></center></body></html>").encode()
+					f"<html><head><title>403</title></head><body><center><h1>Erro 403</h1><br><p>Esta p&aacutegina &eacute restrita.</p></center></body></html>").encode()
 			except Exception as e:
 				return self._headers(500).encode() + htmlfy(
-					f"<html><head><title>UC | 500</title></head><body><center><h1>Erro 500</h1><br><p>Um erro occoreu no servidor. detalhes:<br>{e}</p></center></body></html>").encode()
+					f"<html><head><title>500</title></head><body><center><h1>Erro 500</h1><br><p>Um erro occoreu no servidor. detalhes:<br>{e}</p></center></body></html>").encode()
 		elif type == "POST":
 			values = {"cookies": {}, "ip": addr}
 			for line in body.split("\n"):
@@ -191,7 +226,9 @@ class WebServer(object):
 				data = data.decode('utf-8')
 			except Exception as e:
 				client.close()
-				raise exceptions.DecodeError()
+				for event_function in self.events.hooks["requests"]:
+					event_function(None, data, addr, None, None)
+				break
 			method = data.split(" ")[0]
 			response = self._request_handler(method, data, addr[0])
 			client.send(response)
