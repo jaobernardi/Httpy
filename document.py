e06480e8eac5627d46581e6497ad994775ca70f2
Jão <joao_bernrdi@outlook.com>
2020-09-07 03:47:55 -0300

    Bug fix

--- document.py
+++ document.py
@@ -7,6 +7,7 @@ class Document:
     def __init__(self, data):
         self.data = data
         self._type = None
+        self.extension
 
     @property
     def type(self):
@@ -24,6 +25,39 @@ class Document:
         extension = os.path.basename(path).split(".")[-1]
         if extension.lower() in MIMETypes.types:
             x._type = MIMETypes.types[extension.lower()]
+            x.extension = extension
         file.close()
         return x
+    
+    def parse_python(self, vars):
+        def execute(code, _globals={}, _locals={}):
+            fake_stdout = StringIO()
+            __stdout = sys.stdout
+            sys.stdout = fake_stdout
+            if not 'output' in _globals:
+                _globals['output'] = ''
+            try:
+                ret = eval(code, _globals, _locals)
+                result = fake_stdout.getvalue()
+                sys.stdout = __stdout
+                if ret:
+                    result += str(ret)
+                return f"{_globals['output']}", _globals
+            except:
+                try:
+                    exec(code, _globals)
+                except:
+                    sys.stdout = __stdout			
+                    buf = StringIO()
+                    return f"{_globals['output']}", _globals
+                else:
+                    sys.stdout = __stdout
+                    return f"{_globals['output']}", _globals
+        soup = BeautifulSoup(self.data.decode("utf-8"), 'html.parser')
+        u = str(soup)
+        b = {}
+        for pythontag in soup.find_all("python"):
+            (exec_, b) = execute(str(pythontag).replace("<python>", "").replace("</python>", ""), vars)
+            u = u.replace(str(pythontag), exec_)
+        self.data = u.encode()
 
