e06d73d5fa98edd842a120257409f75144ccc76c
Jão <joao_bernrdi@outlook.com>
2020-09-10 04:56:30 -0300

    Bug fix

--- servers.py
+++ servers.py
@@ -53,9 +53,12 @@ class Server:
 
     def _call_methods(self, method: RequestMethod, route, request):
         host = request.headers["Host"] if "Host" in request.headers else "*"
+        
         if method not in self.functions:
             method = RequestMethod.ANY
-            
+        
+        print(method, self.functions)
+        
         if method in self.functions:
             if host not in self.functions[method]:
                 host = "*"
